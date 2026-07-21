1ad40915bf54b027df1f11880e6cfb104587fd45
IMRAN AHAMAD <imran27386@gmail.com>
2026-07-21 20:46:53 +0530

    new Firebase project waitless-online-token, country support, Vercel env config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,11 +3,11 @@ importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyC6j-YXhHMzuVsw-5Na9u7bB90X7uzLM6M",
-  authDomain: "waitless-online.firebaseapp.com",
-  projectId: "waitless-online",
-  messagingSenderId: "347219606371",
-  appId: "1:347219606371:web:fd3033c83d8612874ab156",
+  apiKey: "AIzaSyAJDyIH-OBbVRSKYxmRDX50S7vkbqY8hrc",
+  authDomain: "waitless-online-token.firebaseapp.com",
+  projectId: "waitless-online-token",
+  messagingSenderId: "679602602855",
+  appId: "1:679602602855:web:80d795df85cf1dc9dc25c2",
 });
 
 // With a `notification` payload, the SW auto-displays it in the background.
